67ca90da524791fbeade7b92f8104e507b65c046
RockPaperScissors <malikmouthif576@gmail.com>
2025-06-18 12:52:13 -0400

    Added buttons and win tracker
    added buttons

--- main.js
+++ main.js
@@ -12,6 +12,24 @@
 const choices = ["rock", "paper", "scissors"]
 let winners = [];
 
+
+
+function resetGame() {
+
+}
+ 
+function startGame() {
+    let imgs = document.querySelectorAll('img')
+    imgs.forEach((img) =>
+    img.addEventListener('click', () =>{
+    if(img.id){
+        playRound(img.id);
+        }
+    }) 
+    );
+}
+
+
 function game(){
     for (let i = 1; i<=5; i++){
         playRound();
@@ -20,12 +38,50 @@ function game(){
     logWins();
 }
 
-function playRound(round) {
-    const playerSelection = playerChoice();
-    const computerSelection = computerChoice();
-    const winner = checkWinner(playerSelection, computerSelection);
+function playRound(playersChoice){
+    const computerChoice = computerSelect();
+    if (wins >= 5){
+        return
+    }
+    const winner = checkWinner(playerChoice, computerChoice)
+
     winners.push(winner);
     logRound(playerSelection, computerSelection, winner, round);
+    tallyWins();
+    displayRound(playerChoice,computerChoice,winner);
+    wins = checkWins();
+    if(wins ==5){
+        displayEnd()
+    }
+}
+
+
+function displayEnd(){
+    let playerWins = winners.filter((item) => item == "Player").length;
+
+    if (playerWins == 5){
+        document.querySelector('.winner').textContent = 'You Won 5 Games'
+    } else {
+        document.querySelector('.winner').textContent = 'Sorry the computer won 5 times'
+    }
+
+    document.querySelector('.reset').style.display = 'flex';
+}
+
+function displayRound(playerChoice,computerChoice,winner){
+    document.querySelector(".playerChoice").textContent = `You Chose: ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)}`;
+    document.querySelector(".computerChoice").textContent = `Computer Chose: ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`;
+    document.querySelector('.winner').textContent = `Round Winner: ${winner}`;
+
+}
+
+function tallyWins(){
+    const pWinCount = winners.filter((item) => item == "Player").length;
+    const cWinCount = winners.filter((item) => item == "Computer").length
+    const ties = winners.filter((item) => item == "Tie").length
+     document.querySelector(".playerScore").textContent = `Score: ${pWinCount}`;
+     document.querySelector(".computerScore").textContent = `Score: ${cWinCountWin}`;
+     document.querySelector(".ties").textContent = `Score: ${ties}`;
 }
 
 function playerChoice(){
@@ -49,6 +105,12 @@ function playerChoice(){
 
 function computerChoice(){
     return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function checkWins(){
+    const pWinCount = winners.filter((item) => item == "Player").length
+    const cWinCount = winners.filter((item) => item == "Computer").length
+    return Math.max(pWinCount,cWinCount)
 }
 
 function validateInput(choice){
